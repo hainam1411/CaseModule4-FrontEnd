30d00d71368fd574c6d53e2117f725c04d5ac4f1
nmtien11111999 <nmtien11111999@gmail.com>
2024-07-09 10:27:36 +0700

    userFood

---
+++ js/productUserShowFood.js
@@ -0,0 +1,22 @@
+function productUserShowFood() {
+    axios.get('http://localhost:8080/products/food')
+        .then(function (response) {
+            let product = response.data;
+            let html = `<table border="1">
+                             <tr>
+                                 <th>Image</th>
+                                 <th>Name</th>
+                                 <th>Price</th>
+                             </tr>`;
+            for (let i = 0; i < product.length; i++) {
+                html += `<tr>
+                                <td><img src="${product[i].image}" alt=""></td>
+                                <td>${product[i].name}</td>
+                                <td>${product[i].price}</td>
+                                <td><button onclick="showFromOrderFood(${product[i].id})">Gọi đồ</button></td>
+                             </tr>`
+            }
+            html += `</table>`;
+            document.getElementById("main").innerHTML = html;
+        })
+}
